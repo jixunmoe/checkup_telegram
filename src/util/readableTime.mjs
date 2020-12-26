52af2a3df4203dbf1812cefe54bf2deca59f906e
Jixun <jixun.moe@gmail.com>
2020-12-26 21:36:20 +0000

    improved time readability

--- src/util/readableTime.mjs
+++ src/util/readableTime.mjs
@@ -1,16 +1,24 @@
-const timeUnit = [1000, 60, 60, 24];
-const timeUnitName = ['ms', ' ', ':', ':'];
+const timeUnits = [
+    // size, suffix, len
+    [1000, 'ms', 3],
+    [60, ' ', 2],
+    [60, ':', 2],
+    [24, ':', 2],
+];
+
 export default function readableTime(ms) {
     let time = ms;
     const result = [];
-    for(let i = 0; i < timeUnit.length && ms > 0; i++) {
-        const unit = timeUnit[i];
-        const name = timeUnitName[i];
-        const value = time % unit;
-        result.push(`${value}${name}`);
-        time = (time / unit) | 0;
+    for(const [size, suffix, len] of timeUnits) {
+        const value = time % size;
+        result.push(`${value.toString().padStart(len, '0')}${suffix}`);
+        time = (time / size) | 0;
+        if (time <= 0) {
+            break;
+        }
     }
     if (time > 0) {
+        // no padding required for days
         result.push(`${time}d `);
     }
     return result.reverse().join('');
